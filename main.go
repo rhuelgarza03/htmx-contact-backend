5609cb7207c03d15b5a403fc338752bc7870d17c
Rhuel Garza <63617583+rhuelgarza03@users.noreply.github.com>
2023-10-28 21:18:55 -0700

    refactor, added app.yaml for app engine

--- main.go
+++ main.go
@@ -14,6 +14,13 @@ type Contact struct {
 }
 
 func main() {
+	http.HandleFunc("/search", handler)
+
+	log.Println("App started on port 8080")
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
 	// hardcoded contacts data
 	contacts := map[string][]Contact{
 		"Contacts": {
@@ -30,43 +37,38 @@ func main() {
 		},
 	}
 
-	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
-		// set headers
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		if r.Method == "GET" {
+	// set headers
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	if r.Method == "GET" {
 
-			// get search string from request
-			tmpl := template.Must(template.ParseFiles("./index.html"))
+		// get search string from request
+		tmpl := template.Must(template.ParseFiles("./index.html"))
 
-			search := strings.ToLower(r.URL.Query().Get("search"))
-			log.Println("search query: ", search)
+		search := strings.ToLower(r.URL.Query().Get("search"))
+		log.Println("search query: ", search)
 
-			// create a map to hold the filtered contacts
-			filtered := make(map[string][]Contact)
+		// create a map to hold the filtered contacts
+		filtered := make(map[string][]Contact)
 
-			// filter out contacts for first matching email, fname, or lname
-			for i := range contacts["Contacts"] {
-				em := strings.ToLower(contacts["Contacts"][i].Email)
-				fn := strings.ToLower(contacts["Contacts"][i].FirstName)
-				ln := strings.ToLower(contacts["Contacts"][i].LastName)
-				if strings.Contains(em, search) || strings.Contains(fn, search) || strings.Contains(ln, search) {
-					filtered["Contacts"] = append(filtered["Contacts"], contacts["Contacts"][i])
-				}
-			}
-
-			log.Println("filtered matches: ", filtered)
-
-			if len(filtered) == 0 {
-				log.Println("no found match for ", search)
-				tmpl.Execute(w, nil)
-			} else {
-				log.Println("found match for ", search)
-				tmpl.Execute(w, filtered)
+		// filter out contacts for first matching email, fname, or lname
+		for i := range contacts["Contacts"] {
+			em := strings.ToLower(contacts["Contacts"][i].Email)
+			fn := strings.ToLower(contacts["Contacts"][i].FirstName)
+			ln := strings.ToLower(contacts["Contacts"][i].LastName)
+			if strings.Contains(em, search) || strings.Contains(fn, search) || strings.Contains(ln, search) {
+				filtered["Contacts"] = append(filtered["Contacts"], contacts["Contacts"][i])
 			}
 		}
-	})
 
-	log.Println("App started on port 8080")
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+		log.Println("filtered matches: ", filtered)
+
+		if len(filtered) == 0 {
+			log.Println("no found match for ", search)
+			tmpl.Execute(w, nil)
+		} else {
+			log.Println("found match for ", search)
+			tmpl.Execute(w, filtered)
+		}
+	}
 }
